39849f24afbec6d97a9228553fa83fb091ec8ad9
Sean Leonard <stleonar@usc.edu>
2015-12-07 11:26:15 -0800

    updated port again

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.get('/numberclean', function (req, res){
 
 
 
-//app.listen('80')
-//onsole.log('Magic happens on port 8000');
+app.listen('8000')
+console.log('Magic happens on port 8000');
 exports = module.exports = app;
