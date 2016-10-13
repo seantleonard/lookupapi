557e339a6de27ecb64a34b38ce8296a3eda8af0c
Sean Leonard <stleonar@usc.edu>
2016-10-13 10:29:37 -0700

    dumb mistake corrected| reset issue on my profile

--- app/telephony.js
+++ app/telephony.js
@@ -7,8 +7,8 @@ module.exports = {
     // These vars are your accountSid and authToken from twilio.com/user/account
 
     getLookupResults: function (searchParam, callback){
-        var accountSid = 'AC3e19a815d95d334d0f5fc6da13dfdde3';
-        var authToken = '52ad68fbfbfe8abf808269de74119405';
+        var accountSid = '';// Your key here
+        var authToken = '';//your key here
         var LookupsClient = require('twilio').LookupsClient;
         var client = new LookupsClient(accountSid, authToken);
 
